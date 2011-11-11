71766a10697e08486d3f072d6ec9127a77934d6f
moderation <michael@sooper.org>
2011-11-11 08:36:17 -0500

    Update hackem.js

--- hackem.js
+++ hackem.js
@@ -43,8 +43,8 @@
 function fullFunc(a){function d(b){if(b.length===0){a.ready();return false}
 $.getScript(b[0],function(){d(b.slice(1))})}function e(b){$.each(b,function(c,f){$("<link>")
 .attr({href:f,rel:"stylesheet",type:'text/css'}).appendTo("head")})}
-var pth = "//ajax.aspnetcdn.com/ajax/jQuery/jquery-1.7.min.js";
-// var pth = "//ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js";
+// var pth = "//ajax.aspnetcdn.com/ajax/jQuery/jquery-1.7.min.js";
+var pth = "//ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js";
 a.jqpath=a.jqpath||pth;
 (function(b){var c=document.createElement("script");c.type="text/javascript";c.src=b;
 c.onload=function(){e(a.css);d(a.js)};document.body.appendChild(c)})(a.jqpath)};
